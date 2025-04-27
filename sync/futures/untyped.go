b7eeec9d52c0cf0b35da9b294364296f15287a89
Vladimir Kochnev <hashtable@yandex.ru>
2025-04-27 13:39:36 +0300

    Add copyright header

--- sync/futures/untyped.go
+++ sync/futures/untyped.go
@@ -1,3 +1,7 @@
+// Copyright 2024-2025 Vladimir Kochnev. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package futures
 
 // UntypedInterface is useful when for example you need a collection of [Future]
